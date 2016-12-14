a65745fa3b1eee59c194c6563ea7bb673b43ae0d
Quinten Peels <quinten@linux.com>
2016-12-14 17:32:38 +0100

    Errorfix Logfile

--- src/Log.java
+++ src/Log.java
@@ -12,22 +12,17 @@ public class Log{
 	public String logDate(){
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
 		LocalDateTime now = LocalDateTime.now();
-		String dateTimeNow = dtf.format(now);
-		return dateTimeNow;
+		String curDate = dtf.format(now);
+		return curDate;
 	}
 
-    public String makeLog(String dateTimeNow, String type, String command) throws IOException {
+    public String makeLog(String curDate, String type, String command) throws IOException {
         BufferedWriter output = new BufferedWriter(new FileWriter("log.txt", true));
-        String logLine = dateTimeNow +""+ type +""+ command;
+        String logLine = curDate +""+ type +""+ command;
         output.append(logLine);
         output.newLine();
         output.close();
         return "done";
     }
-	
-	public String makeLog(String dateTimeNow, String type, String command){
-		return command;
-		
-	}
 
 }
