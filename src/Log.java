2f9c807001127e208ba13adcf3f786714328d5b4
akatar1 <kevin.w@live.nl>
2016-12-14 17:05:45 +0100

    Interface Output verwijderd, Log begonnen

--- src/Log.java
+++ src/Log.java
@@ -1,5 +1,23 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  * Created by Eigenaar on 14-12-2016.
  */
-public class Log implements Output {
+public class Log{
+	
+	public String logDate(){
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+		LocalDateTime now = LocalDateTime.now();
+		String dateTimeNow = dtf.format(now);
+		return dateTimeNow;
+	}
+	
+	
+	
+	public String makeLog(String dateTimeNow, String type, String command){
+		return command;
+		
+	}
+
 }
--- src/Output.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Eigenaar on 14-12-2016.
- */
-public interface Output {
-}
